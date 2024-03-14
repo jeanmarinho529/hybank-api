b245bfbf3598b0b62b18614f1cce805ee7c6a4e9
Jean Marinho <jeanmarinho529@gmail.com>
2024-03-13 21:35:27 -0300

    feat(users): delete user repository

--- app/Repository/UserRepository.php
+++ app/Repository/UserRepository.php
@@ -1,6 +1,14 @@
 <?php
 
 declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
 
 namespace App\Repository;
 
@@ -9,15 +17,10 @@ use Ramsey\Uuid\Uuid;
 
 class UserRepository
 {
-    public function create(array $data): User
+    public function create(array $data)
     {
-        return User::firstOrCreate([
-            'email' => $data['email'],
-        ], [
-            'id' => Uuid::uuid4(),
-            'name' => $data['name'],
-            'password' => password_hash($data['password'], PASSWORD_DEFAULT),
-        ]);
+        $data['id'] = Uuid::uuid4();
+        return User::create($data);
     }
 
     public function find(string $id): User
